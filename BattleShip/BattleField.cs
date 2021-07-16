210884a8098176cd145b646d3bc026ecd2a671d0
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-16 10:25:29 -0700

    cleaned up coded an encapsulated all logic into one class

--- BattleShip/BattleField.cs
+++ BattleShip/BattleField.cs
@@ -18,15 +18,34 @@ namespace BattleShip
         public int EnemyLife;
         public string[,] GridSpace;
 
-        public BattleField(int _enemyRow, int _enemyColumn, string[,] grid)
+        public BattleField()
         {
-            EnemyRow = _enemyRow;
-            EnemyColumnOne = _enemyColumn++;
-            EnemyColumnTwo = _enemyColumn++;
-            EnemyColumnThree = _enemyColumn++;
-            EnemyColumnFour = _enemyColumn++;
-            EnemyColumnFive = _enemyColumn++;
-            GridSpace = grid;
+            static string[,] populateGrid()
+            {
+                string[,] gridTemplate = new string[10, 10];
+
+                for (int i = 0; i < gridTemplate.GetLength(0); i++)
+                {
+                    for (int j = 0; j < gridTemplate.GetLength(1); j++)
+                    {
+                        gridTemplate[i, j] = "-";
+                    }
+                }
+
+                return gridTemplate;
+            }
+            Random random = new Random();
+            int enemyRow = random.Next(0, 10);
+            int enemyColumn = random.Next(0, 5);
+
+
+            EnemyRow = enemyRow;
+            EnemyColumnOne = enemyColumn++;
+            EnemyColumnTwo = enemyColumn++;
+            EnemyColumnThree = enemyColumn++;
+            EnemyColumnFour = enemyColumn++;
+            EnemyColumnFive = enemyColumn++;
+            GridSpace = populateGrid();
             Cannons = 8;
             EnemyLife = 5;
     }
@@ -86,6 +105,59 @@ namespace BattleShip
             drawBoard();
 
             Console.WriteLine("General you {0} the enemy!\n", shotOutcome);
+        }
+
+        public void runGame()
+        {
+            Console.Title = "BATTLESHIP FIRE! Find the Enemy";
+
+        NewGame:
+            Console.Clear();
+            drawBoard();
+
+        Restart:
+
+            try
+            {
+                Console.Write("General please designate Row: ");
+                int cannonRow = Convert.ToInt32(Console.ReadLine());
+                Console.Write("General please designate Column: ");
+                int cannonColumn = Convert.ToInt32(Console.ReadLine());
+
+                checkLocation(cannonColumn, cannonRow);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.Clear();
+                drawBoard();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Please select a number between 0-9\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto Restart;
+            }
+
+            if (Cannons == 0 || EnemyLife == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Game Over!\nPress Y key to play again");
+                Console.ForegroundColor = ConsoleColor.White;
+                if (Console.ReadKey().Key != ConsoleKey.Y)
+                {
+                    Console.WriteLine("Pressed {0}\nTill next time General!", Console.ReadKey().Key);
+                }
+                else
+                {
+                    goto NewGame;
+                }
+
+            }
+            else
+            {
+                goto Restart;
+            }
         }
     }
 }
--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -6,79 +6,12 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
-            Console.Title = "BATTLESHIP FIRE! Find the Enemy";
             
-            
-        NewGame:
-            Console.Clear();
-
-            string[,] grid = populateGrid();
-            Random random = new Random();
-            int enemyRow = random.Next(0, 10);
-            int enemyColumn = random.Next(0, 5);
-           
-            BattleField gameBoard = new BattleField(enemyRow, enemyColumn, grid);
-            gameBoard.drawBoard();
-
-        Restart:
-
-            try
-            {
-                Console.Write("General please designate Row: ");
-                int cannonRow = Convert.ToInt32(Console.ReadLine());
-                Console.Write("General please designate Column: ");
-                int cannonColumn = Convert.ToInt32(Console.ReadLine());
-                
-                gameBoard.checkLocation(cannonColumn, cannonRow);
-   
-            }
-            catch (Exception e)
-            {
-
-                Console.Clear();
-                gameBoard.drawBoard();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Please select a number between 0-9\n");
-                Console.ForegroundColor = ConsoleColor.White;
-                goto Restart;
-            }
-
-            if (gameBoard.Cannons == 0 || gameBoard.EnemyLife == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Game Over!\nPress Y key to play again");
-                Console.ForegroundColor = ConsoleColor.White;
-                if(Console.ReadKey().Key != ConsoleKey.Y)
-                {
-                    Console.WriteLine("Pressed {0}\nTill next time General!", Console.ReadKey().Key);                  
-                } else
-                {               
-                goto NewGame;
-                }
-                
-            }
-            else
-            {
-                    goto Restart;
-            }
+            BattleField gameBoard = new BattleField();
+            gameBoard.runGame();
         }
 
-        public static string[,] populateGrid()
-        {
-            string[,] gridTemplate = new string[10, 10];
-
-            for (int i = 0; i < gridTemplate.GetLength(0); i++)
-            {
-                for (int j = 0; j < gridTemplate.GetLength(1); j++)
-                {
-                    gridTemplate[i, j] = "-";
-                }
-            }
-
-            return gridTemplate;
-        }
-
+        
 
     }
 }
